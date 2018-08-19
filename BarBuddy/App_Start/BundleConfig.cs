e5effd6d4cd9cffb02d36e04aa508d1829d1d5d2
danielfernandez63 <danmanfernan@gmail.com>
2018-08-19 01:43:57 -0500

    added theme bootxtrap and background

--- BarBuddy/App_Start/BundleConfig.cs
+++ BarBuddy/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace BarBuddy
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrapYeti.css",
                       "~/Content/site.css"));
         }
     }
